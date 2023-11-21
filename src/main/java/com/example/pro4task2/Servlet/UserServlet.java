bb3ccde42ee7d51b6c4a7855bc10d78a75591da9
xinyuqiu23 <xqiu@decisionlinks.com>
2023-11-20 23:01:32 -0500

    dchanges

--- src/main/java/com/example/pro4task2/Servlet/UserServlet.java
+++ src/main/java/com/example/pro4task2/Servlet/UserServlet.java
@@ -14,50 +14,18 @@ import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@WebServlet(name = "UserServlet", urlPatterns = {"/user/*"})
+@WebServlet(name = "UserServlet", urlPatterns = {"/user"})
 public class UserServlet extends HttpServlet {
     private final static Logger LOGGER = Logger.getLogger(UserServlet.class.getName());
     private UserService userService = new UserService();
     private Gson gson = new Gson();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path = request.getPathInfo();
-
         // Handle adding a favorite player
-        if ("/addFavorite".equals(path)) {
-            handleAddFavorite(request, response);
-        } else {
-            LOGGER.warning("Invalid POST request path: " + path);
-            System.out.println("Invalid POST request path: " + path);
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid request path");
-        }
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Fetch the default user's information
-        User user = userService.getUser();
-
-        if (user == null) {
-            LOGGER.warning("User not found");
-            System.out.println("User not found");
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, "User not found");
-            return;
-        }
-
-        PrintWriter out = response.getWriter();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        out.print(gson.toJson(user));
-        out.flush();
-        LOGGER.info("User information sent successfully");
-        System.out.println("User information sent successfully");
-    }
-
-    private void handleAddFavorite(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String playerId = request.getParameter("playerId");
 
         if (playerId == null) {
-            LOGGER.log(Level.WARNING, "Player ID is required");
+            LOGGER.log(Level.WARNING, "Player ID is required for POST request");
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Player ID is required");
             return;
         }
@@ -73,4 +41,24 @@ public class UserServlet extends HttpServlet {
         }
     }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Fetch the default user's information
+        User user = userService.getUser();
+
+        if (user == null) {
+            LOGGER.warning("User not found");
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "User not found");
+            return;
+        }
+
+        sendResponse(user, response);
+    }
+
+    private void sendResponse(User user, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(gson.toJson(user));
+        out.flush();
+    }
 }
